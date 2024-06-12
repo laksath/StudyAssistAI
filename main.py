00a81907593857f555a1cff8db87f7c9730d499d
laksath <laksath29112001@gmail.com>
2024-06-13 04:26:18 +0530

    comprehension generator

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from apps import home, youtube_video_quiz, worksheet_generator, mcq_generator
+from apps import home, youtube_video_quiz, worksheet_generator, mcq_generator, comprehension_generator
 
 def main():
     st.sidebar.title("Navigation")
@@ -8,6 +8,7 @@ def main():
         "YouTube Video Summarizer and Quiz Generator": youtube_video_quiz.youtube_video_quiz,
         "Worksheet Generator": worksheet_generator.worksheet_generator,
         "Multiple Choice Assessments": mcq_generator.mcq_generator,
+        "Text Dependent Questions": comprehension_generator.comprehension_generator
     }
     selection = st.sidebar.radio("Go to", list(pages.keys()))
     pages[selection]()
